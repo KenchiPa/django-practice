bbf420d61f5af69881e807c4752d01f5899f5527
KenchiPa <kenchi6734@naver.com>
2023-03-02 10:05:19 +0900

    검색 추가 완료

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -1,5 +1,6 @@
 from django import template
-
+import markdown
+from django.utils.safestring import mark_safe
 register = template.Library()
 
 
@@ -7,3 +8,13 @@ register = template.Library()
 @register.filter
 def sub(value, arg):
     return value - arg  # 기존 값 value에서 입력으로 받은 값 arg를 빼서 return
+
+
+# markdown 모듈과 mark_safe 함수를 이용하여 입력 문자열을 HTML로 변환하는 필터 함수이다.
+# 마크다운에는 몇 가지 확장 기능이 있는데 파이보는 위처럼 nl2br과 fenced_code를 사용하도록 설정.
+# nl2br은 줄바꿈 문자를 <br> 로 바꾸어 준다.
+# fenced_code 는 위에서 살펴본 마크다운의 소스코드 표현을 위해 필요하다.
+@register.filter
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,15 +1,26 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-
+from django.db.models import Q
 from ..models import Question
 
 
 def index(request):
     page = request.GET.get('page', '1')  # 페이지  default
+    kw = request.GET.get('kw', '')  # 검색어
     question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            # filter 함수에서 모델 속성에 접근하기 위해서는 이처럼 __(언더바 2개) 를 이용하여 하위 속성에 접근할 수 있다.
+            # subject__contains=kw 대신 subject__icontains=kw 을 사용하면 대소문자를 가리지 않고 찾아 준다.
+            Q(subject__icontains=kw) |  # 제목 검색 제목에 kw 문자열이 포함되었는지를 의미.
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(answer__content__icontains=kw) |  # 답변 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
+        ).distinct()
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)  # 해당 페이지의 데이터만 조회하도록 쿼리가 변경된다.
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw':kw}
     return render(request, 'pybo/question_list.html', context)
 
 
